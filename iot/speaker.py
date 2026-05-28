ae4953d1f5f4acdf7f78371737b95d45b416a501
linganKR <149742526+linganKR@users.noreply.github.com>
2026-05-28 14:40:55 +0900

    Create speaker.py

---
+++ iot/speaker.py
@@ -0,0 +1,20 @@
+# speaker.py
+
+def play_sound(sound_name):
+
+    print(f"\n[SPEAKER] {sound_name} SOUND PLAY")
+
+
+def play_feed_sound():
+
+    play_sound("FEED")
+
+
+def play_sleep_sound():
+
+    play_sound("SLEEP")
+
+
+def play_happy_sound():
+
+    play_sound("HAPPY")
