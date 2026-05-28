7fbf128ef7c55a37fce3a57d303ebcec15017246
linganKR <149742526+linganKR@users.noreply.github.com>
2026-05-28 14:37:55 +0900

    Create gpio_config.py

---
+++ iot/gpio_config.py
@@ -0,0 +1,33 @@
+# gpio_config.py
+
+# =========================
+# 센서
+# =========================
+
+LIGHT_SENSOR_PIN = 17
+
+TOUCH_SENSOR_PIN = 27
+
+FEED_BUTTON_PIN = 22
+
+PLAY_BUTTON_PIN = 23
+
+SLEEP_BUTTON_PIN = 24
+
+
+# =========================
+# LED 
+# =========================
+
+RED_LED_PIN = 5
+
+GREEN_LED_PIN = 6
+
+BLUE_LED_PIN = 13
+
+
+# =========================
+# 스피커
+# =========================
+
+SPEAKER_PIN = 18
