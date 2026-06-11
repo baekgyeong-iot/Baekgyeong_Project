32b4b2ee6fc6d1c9d48b52b0c36befb03e42d4c5
kimehddud <dud1030898@gmail.com>
2026-06-11 14:27:32 +0900

    Update gpio_config.py

--- iot/gpio_config.py
+++ iot/gpio_config.py
@@ -4,13 +4,9 @@
 # 센서
 # =========================
 
-LIGHT_SENSOR_PIN = 17
+LIGHT_SENSOR_PIN = 22
 
-LEFT_BUTTON_PIN = 23
-
-RIGHT_BUTTON_PIN = 24
-
-GYRO_SENSOR_PIN = 27
+GYRO_SENSOR_PIN = 16
 
 
 
@@ -20,18 +16,22 @@ GYRO_SENSOR_PIN = 27
 # LED 
 # =========================
 
-HUNGER_LED_PIN = 5
+HUNGER_R_PIN = 6
+HUNGER_G_PIN = 18
+HUNGER_B_PIN = 23
 
-ENERGY_LED_PIN = 6
+ENERGY_R_PIN = 2
+ENERGY_G_PIN = 3
+ENERGY_B_PIN = 21
 
-FUN_LED_PIN = 13
+FUN_R_PIN = 5
+FUN_G_PIN = 13
+FUN_B_PIN = 19
 
 
 # =========================
 # 스피커
 # =========================
-
-SPEAKER_PIN = 18
 
 # ========================= 
 # GAME CONFIG 
